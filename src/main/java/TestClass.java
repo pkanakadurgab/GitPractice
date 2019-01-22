9c3380fa2e8f357338a7cd939ad749429234ee32
kanaka <bhavanifriends@gmail.com>
2019-01-22 13:23:03 -0500

    added new java file Test1

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -2,6 +2,6 @@
 public class TestClass {
 	public static void main(String[] args){
 			
-	System.out.println("I made changes to test git show");
+	System.out.println("I made changes to test git diff");
 	}
 }
